83c1a0ea43d6b5475a2d6361dce3b8afc57a177c
Bryan Phelps <bryphe@outlook.com>
2018-06-08 10:16:21 -0700

    Add fog back, make camera parameters more robust

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -4,97 +4,135 @@
  * Scene is the top-level component for a scene
  */
 
-import * as React from "react"
+import * as React from "react";
 
-import * as THREE from "three"
+import * as THREE from "three";
 
-import { Vector3 } from "./Vector"
+import { Vector3 } from "./Vector";
 
-import { createReconciler, ThreeReconcilerCore } from "./Reconciler"
-import { Components } from "./Components"
-import { Scene } from "./Components/Scene"
+import { createReconciler, ThreeReconcilerCore } from "./Reconciler";
+import { Components } from "./Components";
+import { Scene } from "./Components/Scene";
 
 // import { Renderer3dReconciler } from "./Renderer3dReconciler"
-import { NotifyFunction, SceneAndCamera, DisposeFunction } from "./Scene"
+import { NotifyFunction, SceneAndCamera, DisposeFunction } from "./Scene";
 
-import { AudioListenerContext } from "./Components/THREE/AudioListenerContext"
+import { AudioListenerContext } from "./Components/THREE/AudioListenerContext";
 
-const DEFAULT_FOV = 70
+const DEFAULT_FOV = 70;
 
 export interface CameraProps {
-    position: Vector3 
-    lookAt: Vector3
-    fov?: number
-    aspectRatio: number
-    near: number
-    far: number
+  position: Vector3;
+  lookAt: Vector3;
+  fov?: number;
+  aspectRatio: number;
+  near: number;
+  far: number;
 }
 
 export class Camera extends React.PureComponent<CameraProps, {}> {
-    public render(): JSX.Element {
-            return <InnerCamera {...this.props} />
-    }
+  public render(): JSX.Element {
+    return <InnerCamera {...this.props} />;
+  }
 }
 
-
 export interface CameraState {
-    listener: THREE.AudioListener
+  listener: THREE.AudioListener;
 }
 
 export class InnerCamera extends React.PureComponent<CameraProps, CameraState> {
-    private _scene: THREE.Scene
-    private _camera: THREE.PerspectiveCamera
+  private _scene: THREE.Scene;
+  private _camera: THREE.PerspectiveCamera;
 
-    constructor(props: CameraProps) {
-        super(props)
+  constructor(props: CameraProps) {
+    super(props);
 
-        this.state = {
-            listener: null,
-        }
+    this.state = {
+      listener: null
+    };
+  }
+
+  public componentDidMount(): void {
+    if (this._scene) {
+      this._scene.fog = new THREE.Fog(0x000303, 1, 50);
+
+      this._camera = new THREE.PerspectiveCamera(
+        this.props.fov || DEFAULT_FOV,
+        this.props.aspectRatio || 1,
+        this.props.near,
+        this.props.far
+      );
+      this._camera.position.set(
+        this.props.position.x,
+        this.props.position.y,
+        this.props.position.z
+      );
+      this._camera.lookAt(
+        new THREE.Vector3(
+          this.props.lookAt.x,
+          this.props.lookAt.y,
+          this.props.lookAt.z
+        )
+      );
+
+      this._scene.userData = this._camera;
+
+      const listener = new THREE.AudioListener();
+      this._camera.add(listener);
+
+      this.setState({ listener });
+    }
+  }
+
+  public componentDidUpdate(prevProps, prevState) {
+    let shouldUpdateProjectionMatrix = false;
+
+    if (
+      typeof this.props.fov === "number" &&
+      this._camera.fov !== this.props.fov
+    ) {
+      this._camera.fov = this.props.fov;
+      shouldUpdateProjectionMatrix = true;
     }
 
-    public componentDidMount(): void {
-        if (this._scene) {
-
-        
-            this._scene.fog = new THREE.Fog(0x000303, 1, 50)
-
-            this._camera = new THREE.PerspectiveCamera(this.props.fov || DEFAULT_FOV, this.props.aspectRatio || 1, this.props.near, this.props.far)
-            this._camera.position.set(this.props.position.x, this.props.position.y, this.props.position.z)
-            this._camera.lookAt(new THREE.Vector3(this.props.lookAt.x, this.props.lookAt.y, this.props.lookAt.z))
-
-            this._scene.userData = this._camera
-
-            const listener = new THREE.AudioListener()
-            this._camera.add(listener)
-
-            this.setState({listener})
-
-        }
+    if (
+      typeof this.props.aspectRatio === "number" &&
+      this._camera.aspect !== this.props.aspectRatio
+    ) {
+      this._camera.aspect = this.props.aspectRatio;
+      shouldUpdateProjectionMatrix = true;
     }
 
-    public componentDidUpdate(prevProps, prevState) {
-        this._camera.fov = this.props.fov
-        this._camera.aspect  = this.props.aspectRatio
-        this._camera.updateProjectionMatrix()
-
-        if (this.props.position !== prevProps.position) {
-            this._camera.position.set(this.props.position.x, this.props.position.y, this.props.position.z)
-        }
-
-        if (this.props.lookAt !== prevProps.lookAt) {
-            this._camera.lookAt(new THREE.Vector3(this.props.lookAt.x, this.props.lookAt.y, this.props.lookAt.z))
-        }
+    if (shouldUpdateProjectionMatrix) {
+      this._camera.updateProjectionMatrix();
     }
 
-    public render(): JSX.Element {
-        return <Scene ref={(scene) => this._scene = scene}>
-            <AudioListenerContext.Provider value={this.state}>
-            {this.props.children}
-            </AudioListenerContext.Provider>
-        </Scene>
+    if (this.props.position !== prevProps.position) {
+      this._camera.position.set(
+        this.props.position.x,
+        this.props.position.y,
+        this.props.position.z
+      );
     }
 
+    if (this.props.lookAt !== prevProps.lookAt) {
+      this._camera.lookAt(
+        new THREE.Vector3(
+          this.props.lookAt.x,
+          this.props.lookAt.y,
+          this.props.lookAt.z
+        )
+      );
+    }
+  }
+
+  public render(): JSX.Element {
+    return (
+      <Scene ref={scene => (this._scene = scene)}>
+        <AudioListenerContext.Provider value={this.state}>
+          {this.props.children}
+        </AudioListenerContext.Provider>
+      </Scene>
+    );
+  }
 }
-
-
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -4,100 +4,117 @@
  * Scene is the top-level component for a scene
  */
 
-import * as React from "react"
+import * as React from "react";
 
-import * as THREE from "three"
+import * as THREE from "three";
 
-export type Color = number
+export type Color = number;
 
-import { Vector3 } from "./Vector"
+import { Vector3 } from "./Vector";
 
-import { createReconciler, ThreeReconcilerCore } from "./Reconciler"
+import { createReconciler, ThreeReconcilerCore } from "./Reconciler";
 
 export type SceneAndCamera = {
-    scene: THREE.Scene
-    camera: THREE.Camera
-}
+  scene: THREE.Scene;
+  camera: THREE.Camera;
+};
 
-export type DisposeFunction = () => void
-export type NotifyFunction = () => void
+export type DisposeFunction = () => void;
+export type NotifyFunction = () => void;
 
 export namespace Components {
+  export interface RendererProps {
+    width: number;
+    height: number;
+  }
 
-export interface RendererProps {
-    width: number
-    height: number
-}
+  export interface RendererState {
+    sceneAndCameras: SceneAndCamera[];
+  }
 
-export interface RendererState {
-    sceneAndCameras: SceneAndCamera[]
-}
+  export class Renderer extends React.PureComponent<
+    RendererProps,
+    RendererState
+  > {
+    private _mountNode: HTMLElement;
 
-export class Renderer extends React.PureComponent<RendererProps, RendererState> {
-    private _mountNode: HTMLElement
-
-    private _containerElement: HTMLElement
-    private _renderer: THREE.WebGLRenderer
+    private _containerElement: HTMLElement;
+    private _renderer: THREE.WebGLRenderer;
     // private _scene: THREE.Scene
     // private _camera: THREE.Camera
 
-    private _reconciler: any
-    private _rootObject: THREE.Object3D
-    private _shouldRender
+    private _reconciler: any;
+    private _rootObject: THREE.Object3D;
+    private _shouldRender;
 
     constructor(props: RendererProps) {
-        super(props) 
+      super(props);
 
-        this.state = {
-            sceneAndCameras: []
-        }
+      this.state = {
+        sceneAndCameras: []
+      };
     }
 
     public componentDidMount(): void {
-        const { children, width, height } = this.props
-        if (this._containerElement) {
+      const { children, width, height } = this.props;
+      if (this._containerElement) {
+        this._renderer = new THREE.WebGLRenderer({ antialias: true });
+        this._renderer.shadowMap.enabled = true;
+        this._renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this._renderer.autoClear = false;
+        this._renderer.setSize(width, height);
+        this._containerElement.appendChild(this._renderer.domElement);
 
-            this._renderer = new THREE.WebGLRenderer({ antialias: true })
-            this._renderer.shadowMap.enabled = true
-            this._renderer.shadowMap.type = THREE.PCFSoftShadowMap
-            this._renderer.autoClear = false
-            this._renderer.setSize(width, height)
-            this._containerElement.appendChild(this._renderer.domElement)
+        this._rootObject = new THREE.Object3D();
+        const reconcilerCore = new ThreeReconcilerCore();
+        this._reconciler = createReconciler<THREE.Object3D>(reconcilerCore);
 
-            this._rootObject = new THREE.Object3D()
-            const reconcilerCore = new ThreeReconcilerCore()
-            this._reconciler = createReconciler<THREE.Object3D>(reconcilerCore)
+        this._mountNode = this._reconciler.createContainer(this._rootObject);
+        this._reconciler.updateContainer(
+          this.props.children,
+          this._mountNode,
+          this
+        );
 
-            this._mountNode = this._reconciler.createContainer(this._rootObject)
-            this._reconciler.updateContainer(this.props.children, this._mountNode, this)
-        }
+        this._renderScene();
+      }
     }
 
     public componentDidUpdate(prevProps, prevState) {
+      if (
+        this.props.width !== prevProps.width ||
+        this.props.height !== prevProps.height
+      ) {
+        this._renderer.setSize(this.props.width, this.props.height);
+      }
 
-        if (this.props.width !== prevProps.width || this.props.height !== prevProps.height) {
-            this._renderer.setSize(this.props.width, this.props.height)
-        }
+      this._reconciler.updateContainer(
+        this.props.children,
+        this._mountNode,
+        this
+      );
 
-        this._reconciler.updateContainer(this.props.children, this._mountNode, this)
-
-        this._renderScene()
+      this._renderScene();
     }
 
     public render(): JSX.Element {
-            return <div ref={ref => {this._containerElement = ref}} style={{overflow: "hidden"}} />
+      return (
+        <div
+          ref={ref => {
+            this._containerElement = ref;
+          }}
+          style={{ overflow: "hidden" }}
+        />
+      );
     }
 
     private _renderScene(): void {
-        this._rootObject.children.forEach((sc: THREE.Scene) => {
-
-            const camera: THREE.Camera = sc.userData
-            this._renderer.clearDepth()
-            this._renderer.render(sc, camera, null, false)
-
-            
-        })
+      this._renderer.clearColor();
+      this._rootObject.children.forEach((sc: THREE.Scene) => {
+        const camera: THREE.Camera = sc.userData;
+        this._renderer.clearDepth();
+        this._renderer.render(sc, camera, null, false);
+      });
     }
-}
-
+  }
 }
